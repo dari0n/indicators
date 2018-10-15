65d3fb20ba528559d21d59945cd3b5321e09e654
dari0n <79887423414@yandex.ru>
2018-10-15 20:46:45 +0300

    first commit

---
+++ app/Http/Controllers/admin/RedisController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Redis;
+
+class RedisController extends Controller
+{
+    private $startTime;
+    private $execTime;
+
+    public function index(Request $request)
+    {
+
+        $this->startTime = microtime(true); //Start timer
+
+
+
+        $this->getDataFromRedis($request->osn,$request->tf);
+
+        $this->getAllKeys();
+
+    }
+
+    public function UnixTimeStamp($timestamp){
+        return date('H:i:s d-m-Y',substr($timestamp,0,-3));
+    }
+
+    public function getDataFromRedis($osn,$tf)
+    {
+        //Redis::flushdb();
+        $kk = "*:$osn:$tf";
+
+        $i=0;
+        $list = Redis::keys($kk);
+        $data = [];
+        foreach ($list as $key)
+        {
+            $a = 0;
+            //Get Value of Key from Redis
+            $value = Redis::get($key);
+            list($osn, $alt, $open_time, $clouse_time, $clouse_price, $RSI, $ADX, $AO, $MACD, $STOCH_K,$STOCH_D,$STOH_RSI_K,$STOH_RSI_D,$TF,$BIR,$TIME) = explode(":", $value);
+            $data['data'][$i][$a] = $osn;
+            $a++;
+            $data['data'][$i][$a] = $alt;
+            $a++;
+            $data['data'][$i][$a] = $TF;
+            $a++;
+            $data['data'][$i][$a] = $open_time;
+            $a++;
+            $data['data'][$i][$a] = $clouse_time;
+            $a++;
+            $data['data'][$i][$a] = $clouse_price;
+            $a++;
+            $data['data'][$i][$a] = $RSI;
+            $a++;
+            $data['data'][$i][$a] = $ADX;
+            $a++;
+            $data['data'][$i][$a] = $AO;
+            $a++;
+            $data['data'][$i][$a] = $MACD;
+            $a++;
+            $data['data'][$i][$a] = $STOCH_K;
+            $a++;
+            $data['data'][$i][$a] = $STOCH_D;
+            $a++;
+            $data['data'][$i][$a] = $STOH_RSI_K;
+            $a++;
+            $data['data'][$i][$a] = $STOH_RSI_D;
+            $a++;
+            $data['data'][$i][$a] = $TIME;
+
+
+
+            $i++;
+        }
+
+        $this->execTime = round(microtime(true) - $this->startTime);
+
+        print(json_encode($data));
+
+    }
+    public function getAllKeys()
+    {
+
+        $list = Redis::keys("*");
+        $i=0;
+        $keys = [];
+        foreach ($list as $key => $val)
+        {
+            $osn = explode(":",$val);
+            $keys[$i] = $osn[1];
+            $i++;
+        }
+
+        return array_unique($keys);
+
+    }
+
+    function tfInSelect(){
+        $tf = [
+            0 => '5m',
+            1 => '15m',
+            2 => '30m',
+            3 => '1h',
+            4 => '2h',
+            5 => '4h',
+            6 => '6h',
+            7 => '8h',
+            8 => '12h',
+            9 => '1d',
+        ];
+
+        return $tf;
+    }
+
+    public function returnView()
+    {
+        $tf = $this->tfInSelect();
+        $keys = $this->getAllKeys();
+
+        return view('admin.redisView',['keys'=>$keys,'tf'=>$tf]);
+    }
+
+
+
+
+
+
+}
---
+++ resources/views/admin/redisView.blade.php
@@ -0,0 +1,153 @@
+@extends('admin.layout')
+
+@section('content')
+    <div class="box">
+        <div class="box-header">
+            <h3 class="box-title">From Redis-Server *alpha</h3>
+
+        </div>
+        <div class="row">
+           <div style="margin-left: 20px" class="redisSelection">
+               <div class="form-row">
+                   <div class="col-lg-2 col-xs-4">
+
+                       <div class="col">
+                           <label for="getOSN">Choose OSN</label>
+                           <select id="getOSN" style="margin-bottom: 20px" name="OSN" class="form-control">
+                               @foreach($keys as $entry)
+                               <option>{{$entry}}</option>
+                                @endforeach
+                           </select>
+
+                       </div>
+                   </div>
+                   <div class="col-lg-2 col-xs-4">
+                       <div class="col">
+                           <label for="getTF">Choose Timeframe</label>
+                           <select id="getTF"  name="TF" class="form-control">
+                               @foreach($tf as $t)
+                                   <option>{{$t}}</option>
+                               @endforeach
+                           </select>
+
+                       </div>
+                   </div>
+                   <div class="col-lg-2 col-xs-4">
+                       <div class="col">
+                           <label for="getInterval">Reload Interval</label>
+                           <select id="getInterval"  name="interval" class="form-control">
+                                   <option value="60000">1 min</option>
+                                   <option value="120000">2 min</option>
+                                   <option value="180000">3 min</option>
+                                   <option value="240000">4 min</option>
+                                   <option value="300000">5 min</option>
+
+                           </select>
+
+                       </div>
+                   </div>
+                   <div class="col-lg-1 col-xs-4">
+                       <div class="col">
+                           <button type="button" style="margin-top: 23px" id="goRedisButton" onclick="goRedis()" class="btn btn-block btn-success">Go</button>
+                       </div>
+                   </div>
+               </div>
+           </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            <table id="redisTable" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th tabindex="0" id="0">OSN</th>
+                    <th>Pairs</th>
+                    <th>TF</th>
+                    <th>Close Price</th>
+                    <th>Open Time</th>
+                    <th>Clouse Time</th>
+                    <th>RSI</th>
+                    <th>ADX</th>
+                    <th>AO</th>
+                    <th>MACD</th>
+                    <th>STOCH K</th>
+                    <th>STOCH D</th>
+                    <th>STOCH RSI K</th>
+                    <th>STOCH RSI D</th>
+                    <th>Time</th>
+                </tr>
+                </thead>
+                <tbody>
+
+
+
+
+
+                </tbody>
+                <tfoot>
+                <tr>
+                    <th>OSN</th>
+                    <th>Pairs</th>
+                    <th>TF</th>
+                    <th>Close Price</th>
+                    <th>Open Time</th>
+                    <th>Clouse Time</th>
+                    <th>RSI</th>
+                    <th>ADX</th>
+                    <th>AO</th>
+                    <th>MACD</th>
+                    <th>STOCH K</th>
+                    <th>STOCH D</th>
+                    <th>STOCH RSI K</th>
+                    <th>STOCH RSI D</th>
+                    <th>Time</th>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
+        <!-- /.box-body -->
+    </div>
+@endsection
+@section('script')
+    function goRedis() {
+    var osn = $( "#getOSN option:selected" ).text();
+    var tf =  $( "#getTF option:selected" ).text();
+    var RedisTable =   $('#redisTable').DataTable( {
+    "ajax": 'jsonOutput/?osn='+osn+'&tf='+tf,
+    'destroy': true,
+
+    } );
+    setTimeout(function () {
+    if (!RedisTable.data().any()) {
+    alert('Empty data. Please wait. Click GO button after 2 min, or choose another filter.')
+    }
+    },1500);
+    console.log('AutoReload Complete');
+    }
+
+    function firstLoadTable(){
+    var osn = $( "#getOSN option:selected" ).text();
+    var tf =  $( "#getTF option:selected" ).text();
+    setTimeout(function () {
+    var RedisTable =  $('#redisTable').DataTable( {
+    "ajax": 'jsonOutput/?osn='+osn+'&tf='+tf,
+    'destroy': true,
+
+    } );
+    setTimeout(function () {
+    if (!RedisTable.data().any()) {
+    alert('Empty data. Please wait.')
+    }
+    },2000);
+    console.log('Firstload: OK');
+    },1000);
+    }
+
+    function reloadDataTable(){
+    var interval = $( "#getInterval option:selected" ).val();
+    setInterval(function () {
+    goRedis();
+    },interval);
+    }
+    firstLoadTable();
+    reloadDataTable();
+@endsection
