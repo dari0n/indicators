61be812468b74ca4dd217755b1e683c550cf8c5b
dari0n <79887423414@yandex.ru>
2018-10-17 21:49:43 +0300

    0.0.1

---
+++ database/migrations/2018_10_15_180817_visitors.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Visitors extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('session_token')->nullable();
+            $table->integer('group_id')->default(1);
+            $table->integer('is_active')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('visitors');
+    }
+}
